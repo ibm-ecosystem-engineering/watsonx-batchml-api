9dafa2b282e4ba793f4d67e574d0c6a3690ad00a
Sean Sundberg <seansund@us.ibm.com>
2024-05-13 21:07:40 -0500

    Remove unused family-allowance and hello-world services
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,10 +1,7 @@
-import { HelloWorldController } from './hello-world';
 import {CsvDocumentController} from "./csv-document";
 
-export * from './hello-world';
 export * from './csv-document'
 
 export const controllers = [
-    HelloWorldController,
     CsvDocumentController
 ];
